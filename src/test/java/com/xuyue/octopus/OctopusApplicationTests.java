a1179ef68f8b1713ff735c16e031936c1e245130
wangxuyue <921826654@qq.com>
2023-06-02 10:05:40 +0800

    初始化项目-短链系统

---
+++ src/test/java/com/xuyue/octopus/OctopusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyue.octopus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
